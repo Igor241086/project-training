e07e89af1b13a84995f39b423edf55ac7dbcfd4c
Ihor Mahats <igor1999691@gmail.com>
2026-05-05 19:12:58 +0300

    init: create Vite React app, add React Router and Sass, setup base structure

---
+++ new-project/src/AppWrapper.jsx
@@ -0,0 +1,19 @@
+import { App } from "./App.jsx";
+import {
+  ThemeContextProvider,
+  THEME_DARK,
+  ExampleContextProvider,
+} from "@context";
+import { useState } from "react";
+
+export function AppWrapper() {
+  const [theme, setTheme] = useState(THEME_DARK);
+
+  return (
+    <ExampleContextProvider value={{ name: "Serhii" }}>
+      <ThemeContextProvider value={{ theme, setTheme }}>
+        <App />
+      </ThemeContextProvider>
+    </ExampleContextProvider>
+  );
+}
